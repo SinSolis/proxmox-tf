0f4f0244de1ed2561607cd0193d35e26a824c217
juan <juanjose1092@gmail.com>
2023-09-20 22:55:14 -0500

    more resources

--- main.tf
+++ main.tf
@@ -17,14 +17,17 @@ resource "proxmox_vm_qemu" "plex_server" {
   name = "plex01"
   target_node = "pve"
   onboot = true
-  define_connection_info = true
   agent = 1
   clone = "ubuntu-2204-template"
+  ciuser = "juan"
+  ipconfig0 = "ip=dhcp"
+  qemu_os = "l26"
   full_clone = true
   sockets = 1
   cores = 2
-  memory = 2048
+  memory = 8192
   scsihw = "virtio-scsi-pci"
+  sshkeys = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDPo139Zepne8Cu/x4fChh11PM+LphQGxdvuz+zW9qMSy9dzdeXG3g0d4AsxAlEHzrE8mi5G9ne4IPFnSmFg+6F6eZCooz1w5b3D3RspZnlM0bdzGrdf8ddrR7wqhDfLWW6qqx3zZWllB+Zo0zRtczjFi+5M7pdt92ktfmeDAthdEa4GL5JyMH1m1NBWXKlkOLt/uDUAqaW5SH64ruLS87bjWUwfMLxRlNSb7eN4dldk9FZm/qKMNPOv6SbMuySRXwcikaPeOPORJdhIWhhjn2NdGqHeWsW2WaTlk/ypcIvq3db66YaktFf51+ijLpVNz/w/8LKh0k4RAoT3a2FBPhR majora"
   
   #/dev/sda
   disk {
